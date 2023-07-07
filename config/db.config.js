e5c5eeda5cabfbae6900452a5c0e74c1836531c4
Preetisam <preetikamilla@gmail.com>
2023-07-07 20:02:43 +0530

    update

--- config/db.config.js
+++ config/db.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     DB_NAME : "crmClient_DB",
-    DB_URL: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/crmClient_DB"
+    // DB_URL: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/crmClient_DB"
     // DB_URL: "mongodb://127.0.0.1:27017/crm123"
-    // DB_URL: "mongodb://127.0.0.1:27017/crm123"
+    DB_URL: "mongodb://127.0.0.1:2mongodb+srv://preetikamilla:crm123@crmclientticketapp.97e3shf.mongodb.net/7017/crm123"
 
     // mongodb+srv://preetikamilla:<password>@crmclientticketapp.97e3shf.mongodb.net/
 }
